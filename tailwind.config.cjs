d20376644f2f1cd6744622e3559f10cdf50686bc
NafiAsib <nafi.asib@gmail.com>
2023-12-05 14:26:13 +0600

    chore: config file

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "primary-text": "rgb(var(--primary-text-color) / <alpha-value>)",
+        "primary-background": "rgb(var(--primary-bg-color) / <alpha-value>)",
+      },
+    },
+  },
+  plugins: [],
+};
