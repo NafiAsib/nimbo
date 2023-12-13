e8e16ce31032db1c28b2b24512b51f04c15dd0c4
NafiAsib <nafi.asib@gmail.com>
2023-12-14 00:08:32 +0600

    chore: move nav links to separate file

---
+++ src/data/NavLinks.ts
@@ -0,0 +1,14 @@
+export default [
+    {
+      name: 'Experience',
+      url: '#experience'
+    },
+    {
+      name: 'Projects',
+      url: '#projects'
+    },
+    {
+      name: 'Blog',
+      url: 'https://blog.nafiasib.com'
+    }
+  ]
