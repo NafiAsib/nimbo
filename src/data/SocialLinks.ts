db584ae7b19c24433eeebf63320716646dd38ee2
NafiAsib <nafi.asib@gmail.com>
2023-12-14 00:07:16 +0600

    chore: move social links to separate files

---
+++ src/data/SocialLinks.ts
@@ -0,0 +1,22 @@
+export default [
+    {
+      name: 'github',
+      link: 'https://github.com/NafiAsib',
+      icon: '/icons/github.svg'
+    },
+    {
+      name: 'linkedin',
+      link: 'https://www.linkedin.com/in/nafiasib/',
+      icon: '/icons/linkedin.svg'
+    },
+    {
+      name: 'twitter',
+      link: 'https://twitter.com/NafiAsib',
+      icon: '/icons/twitter.svg'
+    },
+    {
+      name: 'facebook',
+      link: 'https://www.facebook.com/nafi.asib',
+      icon: '/icons/facebook.svg'
+    }
+  ]
