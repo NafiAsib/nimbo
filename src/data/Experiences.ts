760e63fdbae113195519e8c54df2c3a3a3fab354
NafiAsib <nafi.asib@gmail.com>
2023-12-14 00:05:24 +0600

    chore: move experience to separate file

---
+++ src/data/Experiences.ts
@@ -0,0 +1,43 @@
+export const experiences = [
+    {
+      title: 'Associate Software Developer',
+      company: '4takeaway',
+      location: 'Mönchengladbach, North Rhine-Westphalia, Germany',
+      startDate: 'May 2023',
+      endDate: 'Present',
+      description:
+        'Developed a social media app with Flutter. Developed a food delivery system. Mentored Junior Software Engineers in Next.js and NestJS',
+      link: 'https://www.4takeaway.com/',
+      skills: ['Next.js', 'Laravel', 'AWS', 'Vercel']
+    },
+    {
+      title: 'Associate Software Developer',
+      company: 'The WOS Germany GmbH',
+      location: 'Mönchengladbach, North Rhine-Westphalia, Germany',
+      startDate: 'Nov 2022',
+      endDate: 'Present',
+      description:
+        'Developed a social media app with Flutter. Developed a food delivery system. Mentored Junior Software Engineers in Next.js and NestJS',
+      link: 'https://thewos.com/',
+      skills: ['Next.js', 'NestJS', 'Flutter', 'AWS']
+    },
+    {
+      title: 'Associate Software Engineer',
+      company: 'Brain Station 23',
+      location: 'Dhaka, Bangladesh',
+      startDate: 'Nov 2021',
+      endDate: 'Nov 2022',
+      description: 'I worked on the Facebook Search team.',
+      link: 'https://brainstation-23.com/',
+      skills: ['React', 'Node.js', 'Next.js']
+    },
+    {
+      title: 'Frontend Developer',
+      company: 'Dast Solution Inc.',
+      location: 'Texas, US',
+      startDate: 'Dec 2020',
+      endDate: 'Jun 2021',
+      description: 'I worked on the Microsoft Search team.',
+      skills: ['Angular', 'Flutter', 'Google Cloud Platform', 'Django']
+    }
+  ]
