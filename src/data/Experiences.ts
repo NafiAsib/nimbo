99252478a1609120c5ebd7da2b6e26fe95067c95
NafiAsib <n.asib@cheq.io>
2024-11-05 23:52:10 +0600

    feat: new experience section

--- src/data/Experiences.ts
+++ src/data/Experiences.ts
@@ -1,43 +1,60 @@
 export default [
-    {
-      title: 'Associate Software Developer',
-      company: '4takeaway',
-      location: 'Mönchengladbach, North Rhine-Westphalia, Germany',
-      startDate: 'May 2023',
-      endDate: 'Present',
-      description:
-        'Developed a social media app with Flutter. Developed a food delivery system. Mentored Junior Software Engineers in Next.js and NestJS',
-      link: 'https://www.4takeaway.com/',
-      skills: ['Next.js', 'Laravel', 'AWS', 'Vercel']
-    },
-    {
-      title: 'Associate Software Developer',
-      company: 'The WOS Germany GmbH',
-      location: 'Mönchengladbach, North Rhine-Westphalia, Germany',
-      startDate: 'Nov 2022',
-      endDate: 'Present',
-      description:
-        'Developed a social media app with Flutter. Developed a food delivery system. Mentored Junior Software Engineers in Next.js and NestJS.',
-      link: 'https://thewos.com/',
-      skills: ['Next.js', 'NestJS', 'Flutter', 'AWS']
-    },
-    {
-      title: 'Associate Software Engineer',
-      company: 'Brain Station 23',
-      location: 'Dhaka, Bangladesh',
-      startDate: 'Nov 2021',
-      endDate: 'Nov 2022',
-      description: 'Worked in an in-house e-commerce project.',
-      link: 'https://brainstation-23.com/',
-      skills: ['React', 'Node.js', 'Next.js']
-    },
-    {
-      title: 'Frontend Developer',
-      company: 'Dast Solution Inc.',
-      location: 'Texas, US',
-      startDate: 'Dec 2020',
-      endDate: 'Jun 2021',
-      description: 'Built an online exam system with Angular. Collaborated with the design team to build a design system & improve product.',
-      skills: ['Angular', 'Flutter', 'Google Cloud Platform', 'Django']
-    }
-  ]
+  {
+    title: 'Software Engineer',
+    company: 'Cheq, a Cantaloupe Platform',
+    location: 'Kirkland, Washington',
+    startDate: 'Jun 2024',
+    endDate: 'Present',
+    description: [
+      'Developing a warehouse management system with Go, Angular & Ionic',
+      'Developend and maintained existing systems with Next.js, Express & Typescript'
+    ],
+    link: 'https://www.cantaloupe.com/solutions/software/cheq-event-management-software/',
+    skills: [
+      'Go',
+      'Angular',
+      'Ionic',
+      'Next.js',
+      'Express',
+      'Typescript',
+      'MySQL'
+    ]
+  },
+  {
+    title: 'Software Developer',
+    company: 'The WOS Germany GmbH',
+    location: 'Mönchengladbach, North Rhine-Westphalia, Germany',
+    startDate: 'Nov 2022',
+    endDate: 'Jun 2024',
+    description: [
+      'Developed a food delivery system with Next.js. Led the frontend team',
+      'Created CI/CD pipelines with GitHub Actions, IaC with Pulumi',
+      'Developed a social media app with Flutter',
+      'Mentored Junior Software Engineers in Next.js and NestJS'
+    ],
+    link: 'https://thewos.com/',
+    skills: ['Next.js', 'NestJS', 'Flutter', 'AWS', 'CI/CD']
+  },
+  {
+    title: 'Associate Software Engineer',
+    company: 'Brain Station 23',
+    location: 'Dhaka, Bangladesh',
+    startDate: 'Nov 2021',
+    endDate: 'Nov 2022',
+    description: ['Worked in an in-house e-commerce project.'],
+    link: 'https://brainstation-23.com/',
+    skills: ['React', 'Node.js', 'Next.js']
+  },
+  {
+    title: 'Frontend Developer',
+    company: 'Dast Solution Inc.',
+    location: 'Texas, US',
+    startDate: 'Dec 2020',
+    endDate: 'Jun 2021',
+    description: [
+      'Built an online exam system with Angular.',
+      'Collaborated with the design team to build a design system & improve product.'
+    ],
+    skills: ['Angular', 'Flutter', 'Google Cloud Platform', 'Django']
+  }
+]
