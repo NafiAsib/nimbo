b7b1fd4dddb8e6adaef835d8f8896130f5696290
NafiAsib <n.asib@cheq.io>
2024-11-10 13:54:01 +0600

    feat: robots.txt & sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
-import { defineConfig } from 'astro/config'
-
-import tailwind from '@astrojs/tailwind'
-
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import sitemap from "@astrojs/sitemap";
+import { site } from './src/data';
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://nafiasib.com',
-  integrations: [tailwind()]
+  site: site,
+  integrations: [
+    tailwind(),
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date()
+    })
+  ]
 })
